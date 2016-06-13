fd838c61110a0c366bf498a0cec50d83ae58aee8
ak <anop72@gmail.com>
2016-06-13 14:30:59 +0700

    add simple http

--- src/app/angular2-starter.component.ts
+++ src/app/angular2-starter.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 @Component({
   moduleId: module.id,
@@ -8,4 +11,12 @@ import { Component } from '@angular/core';
 })
 export class Angular2StarterAppComponent {
   title = 'angular2-starter works!';
+
+  constructor(http: Http) {
+    http.get('http://www.anop72.info/api/playlist/2.json').subscribe(res => {
+        console.log('---- ' + res.json());
+    });
+    console.log('----');
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { Angular2StarterAppComponent, environment } from './app/';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(Angular2StarterAppComponent);
-
+bootstrap(Angular2StarterAppComponent, [HTTP_PROVIDERS]);
