2d5a588ef2513ff145eae0d12a22ae29cdf940e7
ak <anop72@gmail.com>
2016-08-11 23:24:37 +0700

    add seo service and title service
    - inject these to app bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { Title } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
-import { Angular2StarterAppComponent, environment } from './app/';
+import { Angular2StarterAppComponent, environment, SeoService } from './app/';
+
 import { HTTP_PROVIDERS } from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(Angular2StarterAppComponent, [HTTP_PROVIDERS]);
+bootstrap(Angular2StarterAppComponent, [HTTP_PROVIDERS, SeoService, Title]);
